fa76c85053b18844069ab4cf3be2142b53dce701
Lob l1 Support <lobl1support@tmilltd.com>
2024-02-19 12:53:49 +0530

    modified interfece

--- client/src/Pages/ExistingStudent.jsx
+++ client/src/Pages/ExistingStudent.jsx
@@ -4,7 +4,6 @@ import Footer from "../Components/Footer";
 import { Link } from "react-router-dom";
 import postData from "../api/apiService";
 import moment from "moment";
-import UpdateStudent from "../Pages/UpdateStudent";
 
 export default function ExistingStudent() {
   const [Data, setData] = useState([]);
@@ -23,7 +22,7 @@ export default function ExistingStudent() {
     created_dt: moment(new Date()).format("DD-MM-YYYY"),
     flag: "0",
   });
-  
+
   const FetchData = async () => {
     try {
       let Responce = await postData(formData);
@@ -76,24 +75,24 @@ export default function ExistingStudent() {
     <div>
       <Navbar />
       <div className="container mb-5">
-        <div class="container-fluid ">
+        <div className="container-fluid ">
           <section id="minimal-statistics">
-            <div class="row">
-              <div class="col-12 mt-3 mb-1">
-                <h4 class="text-uppercase">Students Area</h4>
+            <div className="row">
+              <div className="col-12 mt-3 mb-1">
+                <h4 className="text-uppercase">Students Area</h4>
                 <p>Manage students and records</p>
               </div>
             </div>
-            <div class="row">
-              <div class="col-xl-3 col-sm-6 col-12">
-                <div class="card">
-                  <div class="card-content">
-                    <div class="card-body">
-                      <div class="media d-flex">
-                        <div class="align-self-center">
-                          <i class="icon-pencil primary font-large-2 float-left"></i>
+            <div className="row">
+              <div className="col-xl-3 col-sm-6 col-12">
+                <div className="card">
+                  <div className="card-content">
+                    <div className="card-body">
+                      <div className="media d-flex">
+                        <div className="align-self-center">
+                          <i className="icon-pencil primary font-large-2 float-left"></i>
                         </div>
-                        <div class="media-body text-right">
+                        <div className="media-body text-right">
                           <h3>{Data ? Data[0]?.v_count : 0}</h3>
                           <span>Number of total Students</span>
                         </div>
@@ -102,15 +101,15 @@ export default function ExistingStudent() {
                   </div>
                 </div>
               </div>
-              <div class="col-xl-3 col-sm-6 col-12">
-                <div class="card">
-                  <div class="card-content">
-                    <div class="card-body">
-                      <div class="media d-flex">
-                        <div class="align-self-center">
-                          <i class="icon-speech warning font-large-2 float-left"></i>
+              <div className="col-xl-3 col-sm-6 col-12">
+                <div className="card">
+                  <div className="card-content">
+                    <div className="card-body">
+                      <div className="media d-flex">
+                        <div className="align-self-center">
+                          <i className="icon-speech warning font-large-2 float-left"></i>
                         </div>
-                        <div class="media-body text-right">
+                        <div className="media-body text-right">
                           <h3>156</h3>
                           <span>New Comments</span>
                         </div>
@@ -119,15 +118,15 @@ export default function ExistingStudent() {
                   </div>
                 </div>
               </div>
-              <div class="col-xl-3 col-sm-6 col-12">
-                <div class="card">
-                  <div class="card-content">
-                    <div class="card-body">
-                      <div class="media d-flex">
-                        <div class="align-self-center">
-                          <i class="icon-graph success font-large-2 float-left"></i>
+              <div className="col-xl-3 col-sm-6 col-12">
+                <div className="card">
+                  <div className="card-content">
+                    <div className="card-body">
+                      <div className="media d-flex">
+                        <div className="align-self-center">
+                          <i className="icon-graph success font-large-2 float-left"></i>
                         </div>
-                        <div class="media-body text-right">
+                        <div className="media-body text-right">
                           <h3>64.89 %</h3>
                           <span>Bounce Rate</span>
                         </div>
@@ -136,15 +135,15 @@ export default function ExistingStudent() {
                   </div>
                 </div>
               </div>
-              <div class="col-xl-3 col-sm-6 col-12">
-                <div class="card">
-                  <div class="card-content">
-                    <div class="card-body">
-                      <div class="media d-flex">
-                        <div class="align-self-center">
-                          <i class="icon-pointer danger font-large-2 float-left"></i>
+              <div className="col-xl-3 col-sm-6 col-12">
+                <div className="card">
+                  <div className="card-content">
+                    <div className="card-body">
+                      <div className="media d-flex">
+                        <div className="align-self-center">
+                          <i className="icon-pointer danger font-large-2 float-left"></i>
                         </div>
-                        <div class="media-body text-right">
+                        <div className="media-body text-right">
                           <h3>423</h3>
                           <span>Total Visits</span>
                         </div>
@@ -158,84 +157,80 @@ export default function ExistingStudent() {
         </div>
       </div>
 
-      {Data && Data.length > 0 ? (
-        <div className="container">
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th scope="col">Serial No</th>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Mobile</th>
-                <th className="col-md-2" scope="col">
-                  Update
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {Data &&
-                Data.length > 0 &&
-                Data.map((row, index) => (
-                  <tr>
-                    <th scope="row">{index + 1}</th>
-                    <td>{row.std_first_name + " " + row.std_last_name}</td>
-                    <td>{row.std_email}</td>
-                    <td>{row.std_mobile}</td>
-                    <td>
-                      <Link to={"/update-student"}>
-                        <button type="button" class="btn btn-warning m-2">
-                          Update
-                        </button>
-                      </Link>
-                      <button
-                        type="button"
-                        class="btn btn-danger"
-                        onClick={() => DeleteItem(index)}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </div>
-      ) : (
-        "No Student Enolled"
-      )}
+      {Data &&
+        Data.length > 0 &&
+        Data.map((row, index) => (
+          <div class="accordion container" id="accordionPanelsStayOpenExample">
+            <div class="accordion-item">
+              <h2
+                class="accordion-header"
+                id={"panelsStayOpen-headingTwo" + index}
+              >
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={"#panelsStayOpen-collapseTwo" + index}
+                  aria-expanded="false"
+                  aria-controls={"panelsStayOpen-collapseTwo" + index}
+                >
+                  <div class="col-1">{index + 1}</div>
+                  <div class="col">
+                    {row.std_first_name + " " + row.std_last_name}
+                  </div>
+                  <div class="col">{row.std_email}</div>
+                  <div class="col">{row.std_mobile}</div>
+                </button>
+              </h2>
+              <div
+                id={"panelsStayOpen-collapseTwo" + index}
+                class="accordion-collapse collapse"
+                aria-labelledby={"panelsStayOpen-headingTwo" + index}
+              >
+                <div class="accordion-body">
+                  <div class="row">
+                    <div class="col">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="First name"
+                        aria-label="First name"
+                      />
+                    </div>
+                    <div class="col">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Last name"
+                        aria-label="Last name"
+                      />
+                    </div>
+                    <div class="col">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Last name"
+                        aria-label="Last name"
+                      />
+                    </div>
+                    <div class="col">
+                      <input
+                        type="text"
+                        class="form-control"
+                        placeholder="Last name"
+                        aria-label="Last name"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
 
-      <nav aria-label="Page navigation example">
-        <ul class="pagination justify-content-center">
-          <li class="page-item">
-            <Link class="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-            </Link>
-          </li>
-          <li class="page-item">
-            <Link class="page-link" href="#">
-              1
-            </Link>
-          </li>
-          <li class="page-item">
-            <Link class="page-link" href="#">
-              2
-            </Link>
-          </li>
-          <li class="page-item">
-            <Link class="page-link" href="#">
-              3
-            </Link>
-          </li>
-          <li class="page-item">
-            <Link class="page-link" href="#" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-            </Link>
-          </li>
-        </ul>
-      </nav>
       {successMessage && (
         <div
-          class="alert alert-success alart-dismissible d-flex justify-content-between w-25 z-3 position-absolute  rounded-3 mx-5"
+          className="alert alert-success alart-dismissible d-flex justify-content-between w-25 z-3 position-absolute  rounded-3 mx-5"
           role="alert"
         >
           {successMessage}
@@ -244,7 +239,7 @@ export default function ExistingStudent() {
             width="16"
             height="16"
             fill="currentColor"
-            class="bi bi-x-circle"
+            className="bi bi-x-circle"
             viewBox="0 0 16 16"
             onClick={closealart}
             style={{ color: "red" }}
